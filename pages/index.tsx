6e55e1ffc778b24056904c2d4686a5c052fc2247
isaack-mungui <munguiisaack@gmail.com>
2022-04-23 22:42:19 +0300

    Build basic layout of the app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,76 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import { useState } from 'react'
+import { FaUser } from 'react-icons/fa'
+import { MdLockOutline } from 'react-icons/md'
 
-const Home: NextPage = () => {
+export default function Home() {
+  const [showPassword, setShowPassword] = useState(false)
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-100">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Binary Pay</title>
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      <main className='flex flex-col items-center justify-center w-full flex-1 px-20 text-center'>
+        <div className="text-center text-3xl font-roboto font-bold mb-4">
+          <span className='text-green-500'>Binary</span>Pay
+        </div>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
+        <div className="bg-white rounded-2xl shadow-2xl w-2/3 max-w-md h-96">
+          <div className='p-5 py-5'>
+            <div className="text-center font-bold text-3xl mb-6 font-roboto">
+              <span className='text-green-500'>Sign in</span>
+            </div>
+            <div className="flex flex-col items-center">
+              <div className="bg-gray-100 w-64 p-2 rounded-lg flex items-center mb-3 ml-4 -mr-4">
+                <FaUser className='text-gray-400 m-2' />
+                <input type="text" placeholder="Username" className='bg-gray-100 font-roboto focus:outline-none text-sm flex-1' />
+              </div>
 
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
+              <div className="bg-gray-100 w-64 p-2 rounded-lg flex items-center mb-3">
+                <MdLockOutline className='text-gray-400 m-2' />
+                <input type={showPassword ? 'text':'password'} placeholder="Password" className='bg-gray-100 peer placeholder-transparent focus:outline-none text-sm flex-1'/>
+                <label
+                 className="absolute
+                  transition-all
+                  left-4
+                  top-1
+                  font-roboto
+                  text-gray-600
+                  text-sm
+                  peer-placeholder-shown:text-gray-400
+                  peer-placeholder-shown:text-base
+                  peer-placeholder-shown:top-4
+                  pointer-events-none
+                  "
+                >
+                  Password
+                </label>
 
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
+                <label
+                  onClick={()=>setShowPassword(!showPassword)}
+                  className=" rounded px-2 
+                    py-1 text-sm 
+                    text-gray-600 
+                    font-roboto
+                    cursor-pointer" 
+                    htmlFor="toggle">{showPassword?'Hide':'Show'}
+                  </label>
+              </div>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+              <div className='flex justify-between w-64 mb-5'>
+                <label className='flex items-center text-xs'>
+                  <input type="checkbox" name="Remember" className='mr-1' />
+                  Remember Password
+                </label>
+                <a href='#' className='text-xs mr-4'>Forgot Password?</a>
+              </div>
+            </div>
+          </div>
         </div>
       </main>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
     </div>
   )
 }
-
-export default Home
